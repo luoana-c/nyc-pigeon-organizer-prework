36994d23ec2ec21463349c05988547b86587ee37
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-10 13:25:47 +0000

    pigeons

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = data[:gender][:male] + data[:gender][:female]
+  
+  pigeon_list = {}
+  pigeons.each do |pigeon|
+    pigeon_list[pigeon] = {}
+  end
+  pigeon_list.each do |pigeon, details|
+    details[:color] = []
+    details[:gender] = []
+    details[:lives] = []
+  end
+  pigeons.each do |pigeon|
+    data.each do |property, type|
+      type.each do |attribute, names|
+        if names.include?(pigeon) 
+          pigeon_list[pigeon][property].push(attribute.to_s)
+        end
+      end
+    end   
+  end
+  pigeon_list
 end
